95adcbbfdb3c2a5fed48db049f6703bca4ddf975
Cubevoid <52008334+Cubevoid@users.noreply.github.com>
2021-01-31 12:48:05 -0500

    Well at least we have *something* going on...

--- supermario.py
+++ supermario.py
@@ -4,7 +4,7 @@ import gym_super_mario_bros
 from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
 import os
 import neat
-import visualize
+# import visualize
 
 xor_inputs = [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0)]
 xor_outputs = [(0.0,), (1.0,), (1.0,), (0.0,)]
@@ -14,6 +14,7 @@ def eval_genomes(genomes, config):
     for genome_id, genome in genomes:
         genome.fitness = 4.0
         net = neat.nn.FeedForwardNetwork.create(genome, config)
+        genome.fitness = play_game()
         for xi, xo in zip(xor_inputs, xor_outputs):
             output = net.activate(xi)
             genome.fitness -= (output[0] - xo[0]) ** 2
@@ -48,26 +49,34 @@ def run(config_file):
         print("input {!r}, expected output {!r}, got {!r}".format(xi, xo, output))
 
     node_names = {-1: 'A', -2: 'B', 0: 'A XOR B'}
-    visualize.draw_net(config, winner, True, node_names=node_names)
-    visualize.plot_stats(stats, ylog=False, view=True)
-    visualize.plot_species(stats, view=True)
+    # visualize.draw_net(config, winner, True, node_names=node_names)
+    # visualize.plot_stats(stats, ylog=False, view=True)
+    # visualize.plot_species(stats, view=True)
 
     p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
     p.run(eval_genomes, 10)
 
 
 def play_game():
+
     env = gym_super_mario_bros.make('SuperMarioBros-v0')
     env = JoypadSpace(env, COMPLEX_MOVEMENT)
 
+    info = {}
+
     done = True
-    for step in range(5000):
+    for step in range(1000):
         if done:
             state = env.reset()
         state, reward, done, info = env.step(env.action_space.sample())  # reward info is returned by step()
-        env.render()
-    print(reward)
+        # env.render()
     env.close()
+
+    if info["flag_get"]:
+        reward = 999999
+
+    print(reward)
+    return reward
 
 
 if __name__ == '__main__':
